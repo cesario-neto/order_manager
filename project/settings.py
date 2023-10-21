4c8c651dd7f2bf3a941ce7d7939dac316f6d6380
Cesario_Neto <cesarioneto7@gmail.com>
2023-10-21 20:26:51 -0300

    Correcting template folder name

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'base_template'],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
