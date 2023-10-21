6f294b883450a88281920945bb255896f0f4a192
Cesario_Neto <cesarioneto7@gmail.com>
2023-10-21 13:08:16 -0300

    Creating and registering in the admin, product model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = 'name', 'price'
+    search_fields = 'name',
--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Produto'
+
+    name = models.CharField(max_length=255, null=False,
+                            blank=False, verbose_name='Nome')
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=False,
+                                blank=False, verbose_name='Preço')
