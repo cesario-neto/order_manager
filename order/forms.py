982ac50513328aae88fdf63e457802b14cda958c
Cesario_Neto <cesarioneto7@gmail.com>
2023-10-22 09:56:32 -0300

    Creating product form

--- order/forms.py
+++ order/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Order
+from .models import Order, ProductOrder
 
 
 class OrderForms(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['client', 'status',]
+
+
+class ProductForms(forms.ModelForm):
+    class Meta:
+        model = ProductOrder
+        fields = ['product', 'quantity']
