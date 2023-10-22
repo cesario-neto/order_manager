4d9b2b053cbb2d33138b3d7d7b262f71a2850d0f
Cesario_Neto <cesarioneto7@gmail.com>
2023-10-21 22:51:50 -0300

    Creating view to create order

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create_order/', views.create_order, name='create_order'),
+    path('order/<int:id>/', views.order_edit, name='order'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,14 +1,22 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, ProductOrder
 
 
 def index(request):
 
     context = {
-        'orders': Order.objects.filter(created_at__gte=date.today())
+        'orders': Order.objects.filter(created_at__gte=date.today()),
     }
 
     return render(request, 'order/index.html', context)
 
-# def
+
+def create_order(request):
+    Order.objects.create()
+
+    return redirect('order:index')
+
+
+def order_edit(request, id):
+    return render(request, 'order/order.html')
