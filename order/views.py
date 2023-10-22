bbb2ee090b4f7b31b5892ace71df15b914570ac6
Cesario_Neto <cesarioneto7@gmail.com>
2023-10-21 22:30:45 -0300

    Creating index view

--- order/views.py
+++ order/views.py
@@ -1,5 +1,14 @@
+from datetime import date
 from django.shortcuts import render
+from .models import Order, ProductOrder
 
 
 def index(request):
-    return render(request, 'order/index.html')
+
+    context = {
+        'orders': Order.objects.filter(created_at__gte=date.today())
+    }
+
+    return render(request, 'order/index.html', context)
+
+# def
