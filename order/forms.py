606322171e4bf12e521643e0b17039b1fe284a3b
Cesario_Neto <cesarioneto7@gmail.com>
2023-11-27 13:02:26 -0300

    adapting new templates

--- order/forms.py
+++ order/forms.py
@@ -6,9 +6,27 @@ class OrderForms(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['client', 'status',]
+        widgets = {
+            'client': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select form-select-sm',
+            })
+        }
 
 
 class ProductForms(forms.ModelForm):
     class Meta:
         model = ProductOrder
         fields = ['product', 'quantity']
+        widgets = {
+            'product': forms.Select(attrs={
+                'class': 'form-select form-select-sm',
+            }),
+            'quantity': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': '1',
+
+            }),
+        }
