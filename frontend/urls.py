86f61867bf54edbb1fedbd740684c743d4060699
Cesario_Neto <cesarioneto7@gmail.com>
2023-11-27 15:28:47 -0300

    changing order display and add pagination

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_order/', views.create_order, name='create_order'),
-    path('checkout/<int:order_id>', views.checkout, name='checkout'),
-    path('order/<int:id>/', views.order_edit, name='order'),
-    path('order/<int:id>/delete_order', views.delete_order, name='delete_order'),
-    path('order/<int:id>/product/', views.add_product, name='add_product'),
-    path('order/<int:id>/delete_product/',
+    path('checkout/<order_id>', views.checkout, name='checkout'),
+    path('order/<id>/', views.order_edit, name='order'),
+    path('order/<id>/delete_order', views.delete_order, name='delete_order'),
+    path('order/<id>/product/', views.add_product, name='add_product'),
+    path('order/<id>/delete_product/',
          views.delete_product, name='delete_product'),
 ]
--- frontend/views/dashboard.py
+++ frontend/views/dashboard.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from order.models import Order
-from datetime import date
+from django.core.paginator import Paginator
 
 
 def index(request):
 
+    orders = Order.objects.all().order_by('-created_at').order_by('-status')
+    paginator = Paginator(orders, 9)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'orders': Order.objects.filter(created_at__gte=date.today()),
+        'orders': orders,
+        'page_obj': page_obj,
     }
 
     return render(request, 'order/index.html', context)
