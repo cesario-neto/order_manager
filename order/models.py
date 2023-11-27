710c4dea0ae7aefe6d1669fd17426cb6326f1b1a
Cesario_Neto <cesarioneto7@gmail.com>
2023-11-27 13:27:03 -0300

    preparing migrates

--- order/models.py
+++ order/models.py
@@ -37,8 +37,6 @@ class Order(BaseModel):
         verbose_name='Valor Total')
     status = models.CharField(
         max_length=255, choices=choices, default='pendente')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.client
