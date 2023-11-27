140a64f34f248c03babd789e2b7e4155106e6e63
Cesario_Neto <cesarioneto7@gmail.com>
2023-11-27 13:02:45 -0300

    remove app name

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'order'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_order/', views.create_order, name='create_order'),
